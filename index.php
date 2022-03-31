8e0e96e174829b002ce5ce2a2481506e1b885b84
Darly Agostinho <darlyagostinho@gmail.com>
2022-03-31 09:35:41 -0300

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <form class="form_login" action="form.php">
+        <div class="container">
+            <div class="form_wrapper">
+                <h1>Login</h1>
+                <input type="email" name="login_email" id="login_email" placeholder="E-mail" class="form_input">
+                <input type="password" name="login_password" id="login_password" placeholder="Senha" class="form_input">
+                <input type="submit" value="Entrar" class="form_submit">
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
